3df7080e9be77551ba48c6bfe08919814cb8b9cb
Bruno57brb <bruno.2022310863@aluno.iffar.edu.br>
2024-07-30 10:06:26 -0300

    sql

--- sql/assistencia.sql
+++ sql/assistencia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 29-Jul-2024 às 19:58
+-- Tempo de geração: 30-Jul-2024 às 12:37
 -- Versão do servidor: 8.0.31
 -- versão do PHP: 8.0.26
 
@@ -32,19 +32,20 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `nome` varchar(255) NOT NULL,
   `senha` varchar(255) NOT NULL,
   `id_usuario` int NOT NULL AUTO_INCREMENT,
-  `matricula` varchar(255) NOT NULL,
   `nivel` int NOT NULL,
-  PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `CPF` varchar(14) NOT NULL,
+  `primerio_login` tinyint NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Extraindo dados da tabela `usuario`
 --
 
-INSERT INTO `usuario` (`nome`, `senha`, `id_usuario`, `matricula`, `nivel`) VALUES
-('bruno', '555', 5, '555', 1),
-('bruno', '666', 6, '666', 2),
-('22', '222', 7, '222', 3);
+INSERT INTO `usuario` (`nome`, `senha`, `id_usuario`, `nivel`, `CPF`, `primerio_login`, `email`) VALUES
+('bruno', '123', 8, 1, '4907172095', 0, '');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
