6408789ce24ea20fc33acc89dc4462c6dd3293f6
antonio99686 <antonio.2022324018@aluno.iffar.edu.br>
2024-09-10 10:43:38 -0300

    login

--- index.php
+++ index.php
@@ -3,23 +3,50 @@
 
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="css/login.css" rel="stylesheet">
-  <title>Login</title>
-  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+  <img id="olho" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABDUlEQVQ4jd2SvW3DMBBGbwQVKlyo4BGC4FKFS4+TATKCNxAggkeoSpHSRQbwAB7AA7hQoUKFLH6E2qQQHfgHdpo0yQHX8T3exyPR/ytlQ8kOhgV7FvSx9+xglA3lM3DBgh0LPn/onbJhcQ0bv2SHlgVgQa/suFHVkCg7bm5gzB2OyvjlDFdDcoa19etZMN8Qp7oUDPEM2KFV1ZAQO2zPMBERO7Ra4JQNpRa4K4FDS0R0IdneCbQLb4/zh/c7QdH4NL40tPXrovFpjHQr6PJ6yr5hQV80PiUiIm1OKxZ0LICS8TWvpyyOf2DBQQtcXk8Zi3+JcKfNafVsjZ0WfGgJlZZQxZjdwzX+ykf6u/UF0Fwo5Apfcq8AAAAASUVORK5CYII="/>
+  <link rel="shortcut icon" href="img/user.png">
+  <link rel="stylesheet" href="css/style.css">
+  <title>LOGIN</title>
 </head>
 
 <body>
-  <form class="form-signin" action="login.php" method="POST">
-    <h1 class="h3 mb-3 font-weight-normal">Login</h1>
-    <input type="text" id="inputId" name="SIAPE" class="form-control" placeholder="SIAPE" required autofocus>
-    <input type="password" id="inputPassword" name="senha" class="form-control" placeholder="Senha" required>
+  <header></header>
 
-    <div class="checkbox mb-3">
+  <div class="iff">
+    <img src="img/iff.jpg" alt="">
   </div>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
-    <a class="btn btn-lg btn-secondary btn-block" href="crud/formcad.php">Cadastrar</a>
+
+  <form action="" method="">
+    <h1>LOGIN</h1>
+    <label for="SIAPE">SIAPE:</label>
+    <input type="text" name="SIAPE">
+    <label for="SIAPE">SENHA:</label>
+    <input type="password" id="senha"name="senha">
   </form>
+
+  <footer></footer>
+  <script>
+        var senha = $('#senha');
+        var olho = $("#olho");
+
+        olho.mousedown(function () {
+            senha.attr("type", "text");
+        });
+
+        olho.mouseup(function () {
+            senha.attr("type", "password");
+        });
+        // para evitar o problema de arrastar a imagem e a senha continuar exposta, 
+        //citada pelo nosso amigo nos comentários
+        $("#olho").mouseout(function () {
+            $("#senha").attr("type", "password");
+        });
+        
+            </script>
 </body>
 
 </html>
